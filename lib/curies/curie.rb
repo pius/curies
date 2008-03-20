0cc46ad07b403b8f2a0eda34838a7e0b6d54fc05
Pius Uzamere <pius@uyiosa.com>
2008-03-19 20:42:18 -0400

    Added ical, vcard, Creative Commons, and a few other namespaces.

--- lib/curies/curie.rb
+++ lib/curies/curie.rb
@@ -1,8 +1,12 @@
 class Curie
-  DEFAULT_MAPPINGS = {"foaf" => "http://xmlns.com/foaf/0.1/", 
-                      "rdfs" => "http://www.w3.org/2000/01/rdf-schema#",
+  DEFAULT_MAPPINGS = {"cal" => "http://www.w3.org/2002/12/cal/ical#",
+                      "cc" => "http://creativecommons.org/ns#",
+                      "contact" => "http://www.w3.org/2001/vcard-rdf/3.0#",
+                      "dc" => "http://purl.org/dc/elements/1.1/",
+                      "foaf" => "http://xmlns.com/foaf/0.1/", 
                       "rdf" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                      "dc" => "http://purl.org/dc/elements/1.1/" }
+                      "rdfs" => "http://www.w3.org/2000/01/rdf-schema#",
+                      "xsd" => "http://www.w3.org/2001/XMLSchema#" }
   @@mappings = {}
   @@mappings.merge! DEFAULT_MAPPINGS
   
