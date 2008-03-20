b2ef6ce5057c4c0a25cdd73399c6c273eadb8a89
Pius Uzamere <pius@uyiosa.com>
2008-03-19 20:25:42 -0400

    added the Dublin Core namespace

--- lib/curies/curie.rb
+++ lib/curies/curie.rb
@@ -1,7 +1,8 @@
 class Curie
   DEFAULT_MAPPINGS = {"foaf" => "http://xmlns.com/foaf/0.1/", 
                       "rdfs" => "http://www.w3.org/2000/01/rdf-schema#",
-                      "rdf" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#" }
+                      "rdf" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                      "dc" => "http://purl.org/dc/elements/1.1/" }
   @@mappings = {}
   @@mappings.merge! DEFAULT_MAPPINGS
   
